9d45e68cc8f97a84a6659a22907e3d1c15c17b02
Zach Wegrzyniak <zwegrzyniak@gmail.com>
2019-07-12 18:52:39 -0600

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// https://stackoverflow.com/a/9733420/1924257
+import React from 'react'
+import './App.css'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface state {
+  colors: string[]
 }
 
-export default App;
+const initialState: state = {
+  colors: ['000000', 'FFFFFF']
+}
+
+type action =
+  | { type: 'editColor'; index: number; value: string }
+  | {
+      type: 'addColor'
+    }
+  | { type: 'removeColor'; index: number }
+
+const ColorEntry: React.FC<{
+  value: string
+  index: number
+  onChange: (value: string, index: number) => void
+}> = React.memo(({ value, index, onChange }) => {
+  return (
+    <div className="color-entry">
+      <div
+        className="swatch"
+        style={{
+          backgroundColor: `${value.startsWith('#') ? '' : '#'}${value}`
+        }}
+      />
+      <input
+        value={value}
+        onChange={event => onChange(event.target.value, index)}
+      />
+    </div>
+  )
+})
+
+function reducer(state: state, action: action) {
+  switch (action.type) {
+    case 'addColor':
+      return {
+        ...state,
+        colors: [...state.colors, '']
+      }
+    case 'editColor':
+      const newColors = [...state.colors]
+
+      newColors[action.index] = action.value
+
+      return {
+        ...state,
+        colors: newColors
+      }
+    case 'removeColor':
+      return {
+        ...state,
+        colors: state.colors.filter((_, index) => index !== action.index)
+      }
+  }
+}
+
+export default () => {
+  const [state, dispatch] = React.useReducer(reducer, initialState)
+
+  const editColor = React.useCallback((value: string, index: number) => {
+    dispatch({ type: 'editColor', value, index })
+  }, [])
+
+  const { colors } = state
+
+  return (
+    <div>
+      <div className="colors">
+        {colors.map((color, index) => (
+          <ColorEntry
+            key={index}
+            value={color}
+            index={index}
+            onChange={editColor}
+          />
+        ))}
+      </div>
+      <button
+        className="add-button"
+        type="button"
+        onClick={() => dispatch({ type: 'addColor' })}
+      >
+        Add color
+      </button>
+    </div>
+  )
+}
